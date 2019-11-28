761903520bb40b33ac4066d398cd9c093d113170
Satchit <32086113+Satchit4@users.noreply.github.com>
2019-11-29 00:58:15 +0530

    Add files via upload

---
+++ node48.m
@@ -0,0 +1,25 @@
+function node=node48 (Nx, Ny, x0, xf, y0, yf)
+%Step3:Assigning global coordinates to nodes
+% Nx=Number of elments in x-direction
+% Ny=Number of elements in y direction
+% Lx=Length of rectanglar domain in x direction
+% Ly=Length of rectanglar domain in y direction
+% node4= coordinates of 4 node elements
+% node8= coordinates of 8 node elements
+dx= (xf-x0) /Nx; dy=(yf-y0) /Ny;
+x1=x0:dx/2:xf;
+y1=y0:dy/2:yf;
+x2=dx/2:dx:xf;
+y2=dy/2:dy:yf;
+[X1, Y1] =meshgrid(y1,x1);
+node1=[Y1(:) X1(:)];
+[X2, Y2]=meshgrid(y2,x2);
+node2=[Y2(:) X2(:)];
+node1=num2str(node1);
+node2=num2str(node2) ;
+node=sortrows(setdiff(node1,node2,'rows'),2);
+node=str2num(node) ;
+node=sortrows(node, 2);
+node=sortrows(node,1);
+node=sortrows(node, 2);
+end
